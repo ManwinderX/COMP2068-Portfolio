1508192f371949e0d9113e9bfa51e43a576e152e
Manwinder Singh <200491908@student.georgianc.on.ca>
2022-10-09 18:43:51 -0500

    finished About page

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Manwinder Singh' });
 });
 
+router.get('/about', (req, res) => {
+  res.render('about',{
+    title: 'About'
+  })
+})
+
+router.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+router.get('/projects', (req, res) => {
+  res.render('projects')
+})
+
 module.exports = router;
